310039e430954f9240b83b99044c86f7fcfc90b1
shuvangkar <sshuvo93@gmail.com>
2020-10-13 12:38:35 +0600

    started refactoring

--- src/FlashMemory.h
+++ src/FlashMemory.h
@@ -9,13 +9,14 @@ class Flash
   public:
     Flash(byte chipSelect);
     Flash(byte CS, uint32_t startAddr, uint16_t packetSz);
+    void begin();
     void setFlashSize(byte sizeMbit);
     void printPage(unsigned int pageNum);
     void printPageBytes(byte *pageBuf);
     void printBytes(byte *buf, byte len);
     bool writeBytes(uint32_t logicalAddr, byte *data, byte length);
     bool readBytes(uint32_t logicalAddr, byte *data, byte length);
-    void begin();
+    
     byte  *_readPage(unsigned int pageNum, byte *page_buffer);
     void _writePage(unsigned int PageNum, byte *pageBuf);
     void eraseChipData();
