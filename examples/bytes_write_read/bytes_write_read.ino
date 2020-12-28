f169cc687cd4b3cb0fb35184e313d0e59207dbb9
shuvangkar <sshuvo93@gmail.com>
2020-12-28 17:52:48 +0600

    example updated

--- examples/bytes_write_read/bytes_write_read.ino
+++ examples/bytes_write_read/bytes_write_read.ino
@@ -1,34 +1,40 @@
 #include "FlashMemory.h"
 
-Flash flash(5); //CS PIN = 10
+#define FLASH_CS 9
+#define OTHER_CS 7 //consider other spi device is connected in bus
+#define FLASH_W_ADDR 700
 
-byte arr_to_write[] = {20,30,40,50,60,70};
+Flash flash(FLASH_CS);
+
+byte arr_to_write[] = {1, 2, 3, 4, 5, 6};
 byte readArr[8];
+
 void setup(void)
 {
-  pinMode(9,OUTPUT);
-  pinMode(10, OUTPUT);
-  digitalWrite(9,HIGH);
-  digitalWrite(10, LOW);
-  Serial.begin(9600);
-  Serial.println("");
+  pinMode(OTHER_CS, OUTPUT);
+  digitalWrite(OTHER_CS, HIGH);
+  Serial.begin(250000);
+
   flash.begin();
   Serial.println("Ready");
+
   //flash.eraseChipData();
   //flash.printPage(10);
-  Serial.println("Writing few bytes in 6016 address");
-  flash.writeBytes(300, arr_to_write, sizeof(arr_to_write));
-  Serial.println("Reading bytes from 6016 address");
-  flash.readBytes(300, readArr, sizeof(readArr));
+
+  Serial.println("Writing bytes");
+  flash.write(FLASH_W_ADDR, arr_to_write, sizeof(arr_to_write));
+
+  Serial.println("Reading bytes");
+  flash.read(FLASH_W_ADDR, readArr, sizeof(readArr));
+  flash.printBytes(readArr, sizeof(readArr));
+
   Serial.println("Setup done");
 }
 
-void loop(void) 
+void loop(void)
 {
   //flash.printPage(19);
   //flash.readBytes(5000, readArr, sizeof(readArr));
   delay(3000);
 
 }
-
-
